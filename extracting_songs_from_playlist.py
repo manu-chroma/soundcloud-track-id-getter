1a67aabf97ea5ce19e192305c35f608938cef7a4
Manvendra Singh <Manu-chroma@users.noreply.github.com>
2016-01-01 01:48:45 +0530

    Takes soundcloud playlist link as input

--- extracting_songs_from_playlist.py
+++ extracting_songs_from_playlist.py
@@ -1,16 +1,24 @@
 #Deps: pip install soundcloud
 import soundcloud
 import json
-client = soundcloud.Client(client_id="YOUR_CLIENT_ID")
-//Playlist id =150058341 for mann ki baat sepetember 2015
-playlist = client.get('/playlists/150058341/')
+client = soundcloud.Client(client_id='852f18e21a0f5fff8061415fdbb04691')
 
-print playlist.title
+link = raw_input("enter playlist link from soundcloud ")
+res = client.get('/resolve', url=link)
+
+playlist = client.get("/playlists/"+str(res.id)+"/")
+
+print "Title of your playlist"
+print playlist.title 
+print "\n"
 yo =  json.dumps(playlist.tracks) 
 aye = json.loads(yo)
-i = 0 
 
 #method of printing from json data is a bit hacky
+print "Playlist content:"
 for i in xrange(yo.count('bpm')):
 	print (i,aye[i]["title"],aye[i]["id"])
+	
+
+
 	
